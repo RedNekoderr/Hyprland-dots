6b0d3c39f7b0b24056830818f1ec5512afdf8d54
RedNekoderr <kirill.aicelly@yandex.com>
2022-12-25 11:16:07 +0300

    POOF!

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-	pfetch
+	nitch
 	starship init fish | source &
 	thefuck --alias | source &
 	~/.config/fish/tty.sh &
